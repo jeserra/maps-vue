01d662ae015d96cfc376d2f01e93c80c258e6584
josermac <joser_isi@hotmail.com>
2018-10-31 23:55:51 -0600

    added files

---
+++ src/views/Map.vue
@@ -0,0 +1,28 @@
+<template>
+   <div id="app" class="container">
+  <div class="row">
+    <div class="col-md-9">
+      <div id="map" class="map"></div>
+    </div>
+    <div class="col-md-3">
+     <div class="form-check"
+       v-for="layer in layers" :key="layer.id">
+  		<label class="form-check-label">
+  <input class="form-check-input" 
+    type="checkbox"
+    v-model="layer.active"
+    @change="layerChanged(layer.id, layer.active)"
+  />
+  {{ layer.name }}
+  </label>
+     </div>
+    </div>
+  </div>
+</div> 
+</template>
+<style scoped>
+.map { height: 600px; }
+</style>
+
+<script src="..\scripts\maps.js"/>
+ 
